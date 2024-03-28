307aab301bb040620c6d28db55b854a390f2587e
Emmanuel Ezeani <emmanuelsavio24@gmail.com>
2024-03-28 21:57:18 +0100

    version 4

--- scroll.js
+++ scroll.js
@@ -60,13 +60,4 @@ elements.forEach(element => {
   checker.observe(element);
 })
 
-// var icon = document.getElementById("icon");
-
-// icon.onclick = function(){
-//   document.body.classList.toggle("light-theme");
-//   if(document.body.classList.contains("light-theme")){
-//     icon.src = "images/moon-solid.svg"
-//   } else {
-//     icon.src = "images/sun_image.png"
-//   }
-// }
+var savio;
