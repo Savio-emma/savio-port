547f02cca158f5e6b86058781d7fc8d489033481
Emmanuel Ezeani <emmanuelsavio24@gmail.com>
2024-04-03 00:28:54 +0100

    update update

--- light.js
+++ light.js
@@ -39,3 +39,23 @@ window.addEventListener("scroll", ()=>{
     scrollTop.classList.remove("static")
   }
 })
+
+//this part is the open and close menu for pads and mobiles only!
+
+
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+const mainMenu = document.querySelector(".header-buttons");
+
+
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
+
+function show(){
+  mainMenu.style.display = 'flex'
+  mainMenu.style.top = '0'
+}
+
+function close(){
+  mainMenu.style.top = '-100%'
+}
